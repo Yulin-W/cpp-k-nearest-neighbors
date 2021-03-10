b7a9fd71d85fca599a02cf63048f7b27cabc5d80
Yulin-W <65204338+Yulin-W@users.noreply.github.com>
2021-03-10 19:09:28 +1100

    data generator complete

--- training/data_generator.py
+++ training/data_generator.py
@@ -1,15 +1,62 @@
 import numpy as np
 import pandas as pd
+import random
 
 # Random point generator with x, y bounds as +- specified value from origin
 def rand_point(x_bound, y_bound):
+    random.seed()
+    x = random.uniform(-x_bound, x_bound)
+    y = random.uniform(-y_bound, y_bound)
+    return (x, y)
 
 # Functions for labelling points
 def quadrant(x, y):
-    if x>=0 and y>=0
+    if x>=0 and y>=0:
+        return 0
+    elif x<0 and y>=0:
+        return 1
+    elif x<0 and y<0:
+        return 2
+    else:
+        return 3
 
-def spiral(x, y):
+def rings(x, y):
+    r = np.sqrt(x**2+y**2)
+    if r > 1 and r < 2:
+        return 0
+    elif r > 3 and r < 4:
+        return 1
+    elif r > 5 and r < 6:
+        return 2
+    elif r > 7 and r < 8:
+        return 3
+    else:
+        return 4
 
 def xaxisbound(x, y):
+    if x>=0:
+        return 0
+    else:
+        return 1
 
 def yequalsxbound(x, y):
+    if y>=x:
+        return 0
+    else:
+        return 1
+
+
+# Function for generating data set
+def generate_data(nPoint, labeller, x_bound, y_bound, filename):
+    points = []
+    for i in range(nPoint):
+        point = rand_point(x_bound, y_bound)
+        points.append((*point, labeller(point)))
+    df = pd.DataFrame(points)
+    df.to_csv(filename)
+
+# Running generation
+generate_data(50, quadrant, 10, 10, "quadrant.csv")
+generate_data(50, rings, 10, 10, "rings.csv")
+generate_data(50, xaxisbound, 10, 10, "xaxisbound.csv")
+generate_data(50, yequalsxbound, 10, 10, "yequalsxbound.csv")
